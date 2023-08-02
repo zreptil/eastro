5025ab5b6f565ec46ba2d6a046daf19cb793dc82
Andi <andi@zreptil.de>
2023-08-02 05:59:05 +0200

    + fixed display of summer and main page

--- src/app/components/five-elements/five-elements.component.ts
+++ src/app/components/five-elements/five-elements.component.ts
@@ -26,10 +26,10 @@ export class FiveElementsComponent {
   initSeason(elem: string): void {
     const temp = [];
     const objList: any = {
-      Herbst: {class: 'flake', content: '#', img: 'fall', count: 75, anim: 'fall', min: 10, max: 210},
-      Winter: {class: 'flake', content: '*', img: 'winter', count: 75, anim: 'fall', min: 10, max: 210},
-      Fruehling: {class: 'flake', content: '+', img: 'spring', count: 75, anim: 'raise', min: 10, max: 210},
-      Sommer: {class: 'flake', content: '(..)', img: 'summer', count: 1, anim: 'up', min: 500, max: 500},
+      Herbst: {class: 'flake', img: 'fall', count: 75, anim: 'fall', size: {min: 10, max: 210}, x: {min: -20, max: 100}, y: {min: -20, max: -80}},
+      Winter: {class: 'flake', img: 'winter', count: 75, anim: 'fall', size: {min: 10, max: 210}, x: {min: -20, max: 100}, y: {min: -20, max: -80}},
+      Fruehling: {class: 'flake', img: 'spring', count: 75, anim: 'raise', size: {min: 10, max: 210}, x: {min: -20, max: 100}, y: {min: -20, max: -80}},
+      Sommer: {class: 'flake', img: 'summer', count: 1, anim: 'up', min: 200, size: {min: 500, max: 500}, x: {min: 50, max: 50}, y: {min: -80, max: -80}},
     };
     const obj = objList[GLOBALS.propsForElement(elem)?.find(p => p.name === 'Jahreszeit')?.property];
     const keys = Object.keys(objList);
@@ -46,10 +46,10 @@ export class FiveElementsComponent {
           src.count--;
         }
       }
-      const x = Math.floor(Math.random() * 100);
-      const y = -20 - Math.floor(Math.random() * 80);
+      const x = src.x.min + Math.floor(Math.random() * (src.x.max - src.x.min));
+      const y = src.y.min + Math.floor(Math.random() * (src.y.max - src.y.min));
       const speed = 5 + Math.floor(1 + Math.random() * 50) / 10;
-      const size = src.min + Math.floor(Math.random() * (src.max - src.min));
+      const size = src.size.min + Math.floor(Math.random() * (src.size.max - src.size.min));
       const style = `top:${y}%;left:${x}%;font-size:${size}%;animation:${src.anim} ${speed}s 1s linear normal forwards`;
       temp.push(`<div style="${style}" class="${src.class}"><img style="width:${size / 5}px" src="assets/images/seasons/${src.img}.png" alt="season image"></div>`);
     }
